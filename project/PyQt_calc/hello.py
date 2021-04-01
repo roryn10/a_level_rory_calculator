8e0dff7145a7d0c13aeae7e7460b88ccb42f32f1
ferno <rormcl07@highgateschool.org.uk>
2021-04-01 19:11:43 +0100

    Started uploading research/notes + experimenting with PyQt

--- project/PyQt_calc/hello.py
+++ project/PyQt_calc/hello.py
@@ -1,17 +1,30 @@
 import sys
 
+from PyQt5.QtWidgets import QPushButton
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QLabel
 from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QHBoxLayout
+from PyQt5.QtWidgets import QVBoxLayout
+
+#app = QApplication(sys.argv)
+#window = QWidget()
+#window.setWindowTitle('PyQt5 App')
+#window.setGeometry (100, 100, 280, 80)
+#window.move(600, 15)
+#helloMsg = QLabel('<h1> Hello World!</h1>', parent=window)
+#helloMsg.move(60, 15)
 
 app = QApplication(sys.argv)
-
 window = QWidget()
-window.setWindowTitle('PyQt5 App')
-window.setGeometry (100, 100, 280, 80)
-window.move(600, 15)
-helloMsg = QLabel('<h1> Hello World!</h1>', parent=window)
-helloMsg.move(60, 15)
+window.setWindowTitle('Layout managers demo')
+layout = QHBoxLayout()
+layout.addWidget(QPushButton('Left'))
+layout.addWidget(QPushButton('Centre'))
+layout.addWidget(QPushButton('Right'))
+window.setLayout(layout)
+
+
 
 window.show()
 sys.exit(app.exec_())
