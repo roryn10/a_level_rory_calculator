5e5e689177ab5de988894de72501fff7d351dcef
ferno <rormcl07@highgateschool.org.uk>
2021-04-06 13:04:06 +0100

    Started uploading research/notes + experimenting with PyQt

--- project/PyQt_calc/hello.py
+++ project/PyQt_calc/hello.py
@@ -6,6 +6,10 @@ from PyQt5.QtWidgets import QLabel
 from PyQt5.QtWidgets import QWidget
 from PyQt5.QtWidgets import QHBoxLayout
 from PyQt5.QtWidgets import QVBoxLayout
+from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QStatusBar
+from PyQt5.QtWidgets import QToolBar
+
 
 #app = QApplication(sys.argv)
 #window = QWidget()
@@ -15,16 +19,41 @@ from PyQt5.QtWidgets import QVBoxLayout
 #helloMsg = QLabel('<h1> Hello World!</h1>', parent=window)
 #helloMsg.move(60, 15)
 
-app = QApplication(sys.argv)
-window = QWidget()
-window.setWindowTitle('Layout managers demo')
-layout = QHBoxLayout()
-layout.addWidget(QPushButton('Left'))
-layout.addWidget(QPushButton('Centre'))
-layout.addWidget(QPushButton('Right'))
-window.setLayout(layout)
+#app = QApplication(sys.argv)
+#window = QWidget()
+#window.setWindowTitle('Layout managers demo')
+#layout = QHBoxLayout()
+#layout.addWidget(QPushButton('Left'))
+#layout.addWidget(QPushButton('Centre'))
+#layout.addWidget(QPushButton('Right'))
+#window.setLayout(layout)
 
+class Window(QMainWindow):
+    def __init__(self, parent = None):
+        super().__init__(parent)
+        self.setWindowTitle('My main window')
+        self.setCentralWidget(QLabel("I'm the central widget"))
+        self._createMenu()
+        self._createToolBar()
+        self._createStatusBar()
 
+    def _createMenu(self):
+        self.menu = self.menuBar().addMenu("&Menu")
+        self.menu.addAction('&Exit', self.close)
 
-window.show()
-sys.exit(app.exec_())
+    def _createToolBar(self):
+        tools = QToolBar()
+        self.addToolBar(tools)
+        tools.addAction('Exit', self.close)
+
+    def _createStatusBar(self):
+        status = QStatusBar()
+        status.showMessage("I'm the status bar")
+        self.setStatusBar(status)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    win = Window()
+    win.show()
+    sys.exit(app.exec_())
+
